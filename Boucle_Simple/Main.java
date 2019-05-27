6681365943edcc2d33b5767f4a13a183aeca0f11
Eijja <50170418+Eijja@users.noreply.github.com>
2019-05-27 20:48:07 +0200

    Boucle_Simple

---
+++ Boucle_Simple/Main.java
@@ -0,0 +1,79 @@
+package boucle_simple;
+import java.util.Random;
+public class Main {
+
+	public static void main(String[] args) {
+/*
+// ---------------------------------------------------------------------------------
+
+
+ENONCE :
+
+Boucle simple : Afficher x fois "Ceci est la ligne numéro x" avec x nombre aléatoire compris entre 0 et 100
+Avec : for, while et do...while
+
+*/
+		
+// ---------------------------------------------------------------------------------			
+		
+		
+		// EXERCICE 1 BOUCLE FOR
+		
+		//INITIALISATION DES VALEURS
+		
+		/*
+		int valeurMin=0,valeurMax=10,i=0;
+		
+		// VALEUR RANDOM
+		
+		Random r = new Random();
+		int valeur = valeurMin + r.nextInt(valeurMax - valeurMin);
+
+		
+		for (i=0;i<=valeur;i++) {
+		System.out.println("Ceci est la ligne numéro "+i);
+			}
+}
+}
+			
+// ---------------------------------------------------------------------------------		
+		// EXERCICE 2 BOUCLE while
+		
+		//INITIALISATION DES VALEURS
+		
+		int valeurMin=0,valeurMax=10,i=0;
+		
+		// VALEUR RANDOM
+		
+		Random r = new Random();
+		int valeur = valeurMin + r.nextInt(valeurMax - valeurMin);
+		
+		while (i<=valeur){
+
+			System.out.println("Ceci est la ligne numéro "+i);
+			i=i+1;
+	}
+}
+}	
+		
+// ---------------------------------------------------------------------------------		
+				// EXERCICE 3 BOUCLE Do while
+		
+		//INITIALISATION DES VALEURS
+		
+		int valeurMin=0,valeurMax=10,i=0;
+		
+		// VALEUR RANDOM
+		
+		Random r = new Random();
+		int valeur = valeurMin + r.nextInt(valeurMax - valeurMin);
+		
+		do {
+			System.out.println("Ceci est la ligne numéro "+i);
+			i=i+1;
+			
+		}while(i<=valeur);
+*/		
+}
+}
+
