276942a28990728d3bae83a5cf6de5ebbe3a8ee7
Eijja <50170418+Eijja@users.noreply.github.com>
2019-06-02 14:50:58 +0200

    V2
    V2

--- Boucle_Imbriquées/Main.java
+++ Boucle_Imbriquées/Main.java
@@ -7,16 +7,112 @@ public class Main {
 ENONCE :
 Afficher les tables de multiplications de x à y pour les valeurs de v à w (Exemple : tables de 0 à 11 pour les valeurs de 0 à 10)
 Faire  avec : 
-for/for: OK 
-while/while = PAS OK 
-do...while/do...while=
-for/while = OK
-for/do...while =  presque 
-while/do...while = 
+
+1. for/for: !!!!!
+
+2. while/while = OK
+
+3. while/for  = OK
+
+4. do while/for = OK 
+
+5. while/do...while = OK
+
+6. do...while /do...while = OK
+
+7. for/while = OK
+
+8. for/do..while = PAS SUR !!
+
+
 
 
 //-------------
-		// EXERCICE 2 FOR, FOR
+ 
+	//  1.FOR, FOR
+		
+
+
+
+
+
+
+
+
+
+
+
+
+				
+	}}}
+
+		// 2.WHILE, WHILE 
+		
+				// INITIALISATION 
+				int i=0,j=0;
+				
+				// RANGE VALEUR X
+				int valeurMinX=1,valeurMaxX=5;
+				
+				
+				//RANGE VALEUR Y
+				int valeurMinY=6,valeurMaxY=10;
+				
+				//RANGE VALEUR V		
+				int valeurMinV=1,valeurMaxV=5;
+				
+				//RANGE VALEUR W	 
+				int valeurMinW=6,valeurMaxW=10;
+				
+				// VAL ALEATOIRE X
+				Random x = new Random();
+				int valeurX = valeurMinX + x.nextInt(valeurMaxX - valeurMinX);
+				
+				// VAL ALEATOIRE Y
+				Random y = new Random();
+				int valeurY  = valeurMinY + y.nextInt(valeurMaxY - valeurMinY);
+				
+				// VAL ALEATOIRE V
+				Random v = new Random();
+				int valeurV  = valeurMinV + v.nextInt(valeurMaxV - valeurMinV);		
+				
+				// VAL ALEATOIRE W
+				Random w = new Random();
+				int valeurW  = valeurMinW + w.nextInt(valeurMaxW - valeurMinW);	
+				
+				
+				// CONCATENATION AVEC LES VALEURS ALEA 
+				System.out.println("Les tables de multiplications de >>> "+valeurX+" <<< allant jusqu'à >>> "+valeurY+" <<< pour les valeurs de >>> "+valeurV+" <<< à >>> "+valeurW+" <<< sont les suivantes : ");
+				
+				
+				while (j<valeurY)// VAL MAX DE LA TABLE
+				{j++;
+
+
+				
+				while (j<valeurX) // VAL MIN DE LA TABLE
+				{j++;
+				
+				}
+				System.out.println("TABLE DE "+j);
+				
+				while (i<valeurW) // LE MAX DE REP
+				{i++;
+				
+
+				while (i<valeurV) // LE MIN DE REP
+				{i++;
+				
+				}
+				System.out.println(j+"X"+i+"="+i*j);
+				}
+				i=0;
+				}
+		}
+		}	
+
+
+		//  3.WHILE FOR
 		
 		// INITIALISATION 
 		int j=1;
@@ -54,132 +150,271 @@ while/do...while =
 		// CONCATENATION AVEC LES VALEURS ALEA 
 		System.out.println("Les tables de multiplications de >>> "+valeurX+" <<< allant jusqu'à >>> "+valeurY+" <<< pour les valeurs de >>> "+valeurV+" <<< à >>> "+valeurW+" <<< sont les suivantes : ");
 		
-		
-		System.out.println("La table de "+ valeurY +" est la suivante :");
-		for (j=valeurX; j<=valeurW;j++) 
-			{for (j=valeurV;j<=valeurW;j++)
-				System.out.println(valeurY+" X "+j+" : -->"+valeurY*j);
-
+		while(valeurX<=valeurY)
 			
+		{for (j=valeurV;j<=valeurW;j++)
 			
-		System.out.println("La table de "+ valeurX +" est la suivante : ");
-		for (j=valeurX; j<=valeurW;j++) 
-			{for (j=valeurV;j<=valeurW;j++)
-				System.out.println(valeurX+" X "+j+" : -->"+valeurX*j);
-			}	
-			}	
-										}
-				}
-
-
-		// EXERCICE 2  WHILE, WHILE PAS REUSSI !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-		
-				// INITIALISATION 
-				int j=1;
-				
-				// RANGE VALEUR X
-				int valeurMinX=1,valeurMaxX=5;
-				
-				
-				//RANGE VALEUR Y
-				int valeurMinY=6,valeurMaxY=10;
-				
-				//RANGE VALEUR V		
-				int valeurMinV=1,valeurMaxV=5;
-				
-				//RANGE VALEUR W	 
-				int valeurMinW=6,valeurMaxW=10;
-				
-				// VAL ALEATOIRE X
-				Random x = new Random();
-				int valeurX = valeurMinX + x.nextInt(valeurMaxX - valeurMinX);
-				
-				// VAL ALEATOIRE Y
-				Random y = new Random();
-				int valeurY  = valeurMinY + y.nextInt(valeurMaxY - valeurMinY);
-				
-				// VAL ALEATOIRE V
-				Random v = new Random();
-				int valeurV  = valeurMinV + v.nextInt(valeurMaxV - valeurMinV);		
-				
-				// VAL ALEATOIRE W
-				Random w = new Random();
-				int valeurW  = valeurMinW + w.nextInt(valeurMaxW - valeurMinW);	
-				
-				
-				// CONCATENATION AVEC LES VALEURS ALEA 
-				System.out.println("Les tables de multiplications de >>> "+valeurX+" <<< allant jusqu'à >>> "+valeurY+" <<< pour les valeurs de >>> "+valeurV+" <<< à >>> "+valeurW+" <<< sont les suivantes : ");
-				
-				
-				while (j<=valeurW) 
-					{while (j>=valeurW)
-					System.out.println(valeurY+" X "+j+" : -->"+valeurY*j);
-						j++;
-					
-					}
-					}
+			System.out.println(valeurX+" X "+j+" : -->"+valeurX*j);
+		System.out.println("        ");
+			valeurX++;
 		}
-				
 
-
-					
-
-					
-				
-
-
-// EXERCICE 2  WHILE FOR
+}
+}
 		
-				// INITIALISATION 
-				int j=1;
-				
-				// RANGE VALEUR X
-				int valeurMinX=1,valeurMaxX=5;
-				
-				
-				//RANGE VALEUR Y
-				int valeurMinY=6,valeurMaxY=10;
-				
-				//RANGE VALEUR V		
-				int valeurMinV=1,valeurMaxV=5;
-				
-				//RANGE VALEUR W	 
-				int valeurMinW=6,valeurMaxW=10;
-				
-				// VAL ALEATOIRE X
-				Random x = new Random();
-				int valeurX = valeurMinX + x.nextInt(valeurMaxX - valeurMinX);
-				
-				// VAL ALEATOIRE Y
-				Random y = new Random();
-				int valeurY  = valeurMinY + y.nextInt(valeurMaxY - valeurMinY);
-				
-				// VAL ALEATOIRE V
-				Random v = new Random();
-				int valeurV  = valeurMinV + v.nextInt(valeurMaxV - valeurMinV);		
-				
-				// VAL ALEATOIRE W
-				Random w = new Random();
-				int valeurW  = valeurMinW + w.nextInt(valeurMaxW - valeurMinW);	
-				
-				
-				// CONCATENATION AVEC LES VALEURS ALEA 
-				System.out.println("Les tables de multiplications de >>> "+valeurX+" <<< allant jusqu'à >>> "+valeurY+" <<< pour les valeurs de >>> "+valeurV+" <<< à >>> "+valeurW+" <<< sont les suivantes : ");
-				
+		
+		
 
-				while (j<=valeurW) 
-					{for (j=valeurV;j<=valeurW;j++)
-						System.out.println(valeurY+" X "+j+" : -->"+valeurY*j);
-					}
+			//  4.DO WHILE/FOR
+		
+			// INITIALISATION 
+			int j=1;
+			
+			// RANGE VALEUR X
+			int valeurMinX=1,valeurMaxX=5;
+			
+			
+			//RANGE VALEUR Y
+			int valeurMinY=6,valeurMaxY=10;
+			
+			//RANGE VALEUR V		
+			int valeurMinV=1,valeurMaxV=5;
+			
+			//RANGE VALEUR W	 
+			int valeurMinW=6,valeurMaxW=10;
+			
+			// VAL ALEATOIRE X
+			Random x = new Random();
+			int valeurX = valeurMinX + x.nextInt(valeurMaxX - valeurMinX);
+			
+			// VAL ALEATOIRE Y
+			Random y = new Random();
+			int valeurY  = valeurMinY + y.nextInt(valeurMaxY - valeurMinY);
+			
+			// VAL ALEATOIRE V
+			Random v = new Random();
+			int valeurV  = valeurMinV + v.nextInt(valeurMaxV - valeurMinV);		
+			
+			// VAL ALEATOIRE W
+			Random w = new Random();
+			int valeurW  = valeurMinW + w.nextInt(valeurMaxW - valeurMinW);	
+			
+			
+			// CONCATENATION AVEC LES VALEURS ALEA 
+			System.out.println("Les tables de multiplications de >>> "+valeurX+" <<< allant jusqu'à >>> "+valeurY+" <<< pour les valeurs de >>> "+valeurV+" <<< à >>> "+valeurW+" <<< sont les suivantes : ");
+						
+			do {
+			{for (j=valeurV; j<=valeurW;j++) 
+			  System.out.println(valeurX+" X "+j+" : -->"+valeurX*j);
+			valeurX++;
+
+			}
+			System.out.println("        ");
+			}  while(valeurX<=valeurY);
+			
+			
 	}
+	}
+
+		
+		
+
+				// 5. while/do...while 
+		
+		// INITIALISATION 
+		int i=0,j=0;
+		
+		// RANGE VALEUR X
+		int valeurMinX=1,valeurMaxX=5;
+		
+		
+		//RANGE VALEUR Y
+		int valeurMinY=6,valeurMaxY=10;
+		
+		//RANGE VALEUR V		
+		int valeurMinV=1,valeurMaxV=5;
+		
+		//RANGE VALEUR W	 
+		int valeurMinW=6,valeurMaxW=10;
+		
+		// VAL ALEATOIRE X
+		Random x = new Random();
+		int valeurX = valeurMinX + x.nextInt(valeurMaxX - valeurMinX);
+		
+		// VAL ALEATOIRE Y
+		Random y = new Random();
+		int valeurY  = valeurMinY + y.nextInt(valeurMaxY - valeurMinY);
+		
+		// VAL ALEATOIRE V
+		Random v = new Random();
+		int valeurV  = valeurMinV + v.nextInt(valeurMaxV - valeurMinV);		
+		
+		// VAL ALEATOIRE W
+		Random w = new Random();
+		int valeurW  = valeurMinW + w.nextInt(valeurMaxW - valeurMinW);	
+		
+		
+		// CONCATENATION AVEC LES VALEURS ALEA 
+		System.out.println("Les tables de multiplications de >>> "+valeurX+" <<< allant jusqu'à >>> "+valeurY+" <<< pour les valeurs de >>> "+valeurV+" <<< à >>> "+valeurW+" <<< sont les suivantes : ");
+		
+		
+		while (j<valeurY)// VAL MAX DE LA TABLE
+		{
+
+		do {j++;
+			
+		
+		}while (j<valeurX); // VAL MIN DE LA TABLE
+		
+		
+		
+		System.out.println("TABLE DE "+j);
+		
+		while (i<valeurW) // LE MAX DE REP
+		{
+		
+		do {i++;
+		
+		}while (i<valeurV); // LE MIN DE REP
+		
+		
+		
+		System.out.println(j+"X"+i+"="+i*j);
+		}
+		i=0;
+		}
+}
 }
 
 
+
+		// 6.do...while /do...while 
 		
-// EXERCICE 2  FOR DO WHILE
+		// INITIALISATION 
+		int i=0,j=0;
+		
+		// RANGE VALEUR X
+		int valeurMinX=1,valeurMaxX=5;
+		
+		
+		//RANGE VALEUR Y
+		int valeurMinY=6,valeurMaxY=10;
+		
+		//RANGE VALEUR V		
+		int valeurMinV=1,valeurMaxV=5;
+		
+		//RANGE VALEUR W	 
+		int valeurMinW=6,valeurMaxW=10;
+		
+		// VAL ALEATOIRE X
+		Random x = new Random();
+		int valeurX = valeurMinX + x.nextInt(valeurMaxX - valeurMinX);
+		
+		// VAL ALEATOIRE Y
+		Random y = new Random();
+		int valeurY  = valeurMinY + y.nextInt(valeurMaxY - valeurMinY);
+		
+		// VAL ALEATOIRE V
+		Random v = new Random();
+		int valeurV  = valeurMinV + v.nextInt(valeurMaxV - valeurMinV);		
+		
+		// VAL ALEATOIRE W
+		Random w = new Random();
+		int valeurW  = valeurMinW + w.nextInt(valeurMaxW - valeurMinW);	
+		
+		
+		// CONCATENATION AVEC LES VALEURS ALEA 
+		System.out.println("Les tables de multiplications de >>> "+valeurX+" <<< allant jusqu'à >>> "+valeurY+" <<< pour les valeurs de >>> "+valeurV+" <<< à >>> "+valeurW+" <<< sont les suivantes : ");
+		
+		do {
+
+		do {j++;
+			
+		
+		}while (j<valeurX); // VAL MIN DE LA TABLE
+		
+		
+		
+		System.out.println("TABLE DE "+j);
+		do {
+		
+		
+
+		
+		do {i++;
+		
+		}while (i<valeurV); // LE MIN DE REP
+		
+		
+		
+		System.out.println(j+"X"+i+"="+i*j);
+		}while (i<valeurW); // LE MAX DE REP
+		i=0;
+		
+		}while (j<valeurY);// VAL MAX DE LA TABLE
+}
+}
+
+
+
+		// 7.for/while
+		
+		// INITIALISATION 
+		int j=1,i=0;
+		
+		// RANGE VALEUR X
+		int valeurMinX=1,valeurMaxX=5;
+		
+		
+		//RANGE VALEUR Y
+		int valeurMinY=6,valeurMaxY=10;
+		
+		//RANGE VALEUR V		
+		int valeurMinV=1,valeurMaxV=5;
+		
+		//RANGE VALEUR W	 
+		int valeurMinW=6,valeurMaxW=10;
+		
+		// VAL ALEATOIRE X
+		Random x = new Random();
+		int valeurX = valeurMinX + x.nextInt(valeurMaxX - valeurMinX);
+		
+		// VAL ALEATOIRE Y
+		Random y = new Random();
+		int valeurY  = valeurMinY + y.nextInt(valeurMaxY - valeurMinY);
+		
+		// VAL ALEATOIRE V
+		Random v = new Random();
+		int valeurV  = valeurMinV + v.nextInt(valeurMaxV - valeurMinV);		
+		
+		// VAL ALEATOIRE W
+		Random w = new Random();
+		int valeurW  = valeurMinW + w.nextInt(valeurMaxW - valeurMinW);	
+		
+		
+		// CONCATENATION AVEC LES VALEURS ALEA 
+		System.out.println("Les tables de multiplications de >>> "+valeurX+" <<< allant jusqu'à >>> "+valeurY+" <<< pour les valeurs de >>> "+valeurV+" <<< à >>> "+valeurW+" <<< sont les suivantes : ");
+		
+
+		for (j=valeurX; j<=valeurY;j++) 
+		{System.out.println("TABLE DE "+j);
+		while (i<valeurW) // LE MAX DE REP
+		{i++;
+		while (i<valeurV) // LE MIN DE REP
+		{i++;		
+		}
+		System.out.println(j+"X"+i+"="+i*j);
+		}
+		i=0;
+		}		
+}
+}
+
+
+		// 8.for/do..while
 		
 				// INITIALISATION 
-				int j=1;
+				int j=1,i=0;
 				
 				// RANGE VALEUR X
 				int valeurMinX=1,valeurMaxX=5;
@@ -213,24 +448,22 @@ while/do...while =
 				
 				// CONCATENATION AVEC LES VALEURS ALEA 
 				System.out.println("Les tables de multiplications de >>> "+valeurX+" <<< allant jusqu'à >>> "+valeurY+" <<< pour les valeurs de >>> "+valeurV+" <<< à >>> "+valeurW+" <<< sont les suivantes : ");
-							
+				
+
+				for (j=valeurX; j<=valeurY;j++) 
+				{System.out.println("TABLE DE "+j);
 				do {
-				{for (j=valeurV; j<=valeurW;j++) 
-				  System.out.println(valeurX+" X "+j+" : -->"+valeurX*j);
-				valeurX++;
-
-				}
-				}  while(valeurX<=valeurY);
-				
-}
-}
-	
-
-*/
-				
-
-
-
-
-
-				
+				do {i++;
+					
+				}while (i<valeurV); // LE MIN DE REP
+					
+					
+					
+				System.out.println(j+"X"+i+"="+i*j);
+				}while (i<valeurW); // LE MAX DE REP
+				i=0;
+					
+				}while (j<valeurY);// VAL MAX DE LA TABLE
+			}
+			}
+		*/
